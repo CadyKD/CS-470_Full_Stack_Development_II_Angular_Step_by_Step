cb3e0a48187a675f6ff03ce7ddc9c4aa9a549670
DavidKD <CadyKD@gmail.com>
2023-10-08 13:00:24 -0700

    Updated URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
 //   api_url: 'https://q-a-example-loopback-api.herokuapp.com'
-  api_url: 'http://localhost:3000'
+  api_url: 'https://x2gnetamfb.execute-api.us-east-1.amazonaws.com'
 };
